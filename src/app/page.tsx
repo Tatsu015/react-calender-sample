0a377e322244d395b0697d5a4a0470b09a8b8fa5
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-07-15 14:32:59 +0900

    add select range

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "./calendar.css";
-
 export default function Home() {
+  const today = new Date();
+  const tomorrow = new Date(new Date(today).setDate(today.getDate() + 10));
+
   return (
-    <Calendar
-      value={new Date()}
-      locale="ja-JP"
-      next2Label=""
-      onClickDay={(value, event) => {
-        console.log(value);
-      }}
-    />
+    <div className="app">
+      <h1 className="text-center">React Calendar with Range</h1>
+      <div className="calendar-container">
+        <Calendar selectRange={true} value={[today, tomorrow]} locale="ja-JP" />
+      </div>
+    </div>
   );
 }
